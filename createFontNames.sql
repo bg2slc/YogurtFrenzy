e1a01e5bda96ebbbbb9f5346015c93c8a46f1675
bg2slc <benjamin.garrett2@student.sl.on.ca>
2022-11-08 00:02:05 -0500

    corrected sql script

--- createFontNames.sql
+++ createFontNames.sql
@@ -1,9 +1,12 @@
--- CREATE SCHEMA comp220db;
-DROP 
-use comp220db;
-CREATE TABLE fontName (fontName varchar(50), primary key (fontName));
+DROP DATABASE IF EXISTS comp220db;
+CREATE DATABASE comp220db;
+USE comp220db;
+
+CREATE TABLE fontNames (fontName varchar(50), primary key (fontName));
 INSERT INTO fontNames VALUES ('Arial');
 INSERT INTO fontNames VALUES ('Brush Script MT');
 INSERT INTO fontNames VALUES ('Comic Sans MS');
 INSERT INTO fontNames VALUES ('Courier New');
 INSERT INTO fontNames VALUES ('Verdana');
+
+SELECT * from fontNames;
