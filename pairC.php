0f7fa528187d774e2ef7ab63f77aefddcaef9e13
Emmett <113130079+EmmettS03@users.noreply.github.com>
2022-11-12 13:57:57 -0500

    Update pairC.php

--- pairC.php
+++ pairC.php
@@ -4,7 +4,11 @@
 //findTextInFile
 function findTextInFile() {
   $a = "feline cat";
-  
+  if() {
+      echo "<p>String $a not found</p>";
+  } else {
+      echo "<p>$a was found at position 4.</p>";
+  }
 }
 
 ?>
