1ed293e2e8191fd5e1d85eed0d9924f7e4d8648a
bg2slc <benjamin.garrett2@student.sl.on.ca>
2022-11-12 12:21:37 -0500

    pairA code started

---
+++ pairA.php
@@ -0,0 +1,35 @@
+<?php
+//Robin and Ben's Assignment FIle
+
+
+
+
+//6 functions:
+
+//drawMenu
+
+function drawMenu()
+{
+
+}
+
+
+
+//drawFileDropDown
+//drawEditDropDown
+//drawFontDropDown
+//saveFile - Has 1 parameter
+//openFIle - Has no parameters
+
+
+
+//Filename is editor.dat. Display the messages below
+
+//-File Saved
+//-Error Saving FIle
+//-File opened
+//-Error opening file
+
+
+?>
+
