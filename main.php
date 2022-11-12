0a19bdafc28dae5112f4289cee57d9162ab39e43
bg2slc <benjamin.garrett2@student.sl.on.ca>
2022-11-12 13:29:39 -0500

    brought example code into main.php

---
+++ main.php
@@ -0,0 +1,40 @@
+<!-- // http://localhost/main.php -->
+<?php
+
+//+++++++++++++++++++++ SETUP
+require_once("include.php");
+//ERROR REPORTING - Set to true for error reporting
+//TODO: Disable this when ready for submitting
+if(true)    {
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'On');
+}
+
+//+++++++++++++++++++++ FUNCTIONS
+        /* Write primary PHP functions for use in MAIN here */
+
+//+++++++++++++++++++++ MAIN 
+        /* Main Loop, which should run each time the page is loaded */
+$mysqlObj = createConnectionObject();
+
+writeHeaders("Yogurt Frenzy", "Yogurt Frenzy Notepad");
+
+/*  //Code from Assignment 1, only left here for quick reference
+if (isset($_POST['f_CreateTable']))
+  createTableForm($mysqlObj,$TableName);
+else if (isset($_POST['f_Save'])) 
+    saveRecordtoTableForm($mysqlObj,$TableName);
+else if (isset($_POST['f_AddRecord'])) 
+    addRecordForm($mysqlObj,$TableName);	   
+else if (isset($_POST['f_DeleteRecord']))
+    deleteRecordForm($mysqlObj,$TableName);
+else if (isset($_POST['f_DisplayData']))
+   displayDataForm ($mysqlObj,$TableName);
+else if (isset($_POST['f_IssueDelete']))
+   issueDeleteForm ($mysqlObj,$TableName);
+else displayMainForm();
+ */
+
+if (isset($mysqlObj)) $mysqlObj->close();
+writeFooters();
+?>
