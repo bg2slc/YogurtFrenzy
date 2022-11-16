f041bcbe73869a0053c058f022ff10167096b3dc
duganlang <113129579+duganlang@users.noreply.github.com>
2022-11-16 13:53:45 -0500

    Update pairC.php

--- pairC.php
+++ pairC.php
@@ -1,42 +1,26 @@
 <?php
 //Pair C - Emmett and Dugan
 
-//function findInText($text) {
-  //What to look for
-  $search = '$text';
-  //Read from file
-  $lines = file('file.txt');
-  //Store true when text is found
-  $pos = strpos($string, $lines);
-
-  //Check if the line contains the string we're looking for, and print if it does
-  if($pos === false) {
-    echo "<p>" ."the string: " . $search . "was not foiund in the string: " . $lines . "</p>";
-  } else {
-    echo "the string: " . $search . "was found in the string" . $lines;
-    echo " and exists at position " . $pos;
-  }
-}
-?>
-
-<?php
-//Pair C - Emmett and Dugan
-
 function findTextinFile($string, $text)
 {
-  $mystring=$string;
-
+  //text area
+  $mystring= $string;
+  //text to search
   $findme=$text;
   
-  $pos=strpos($mystring, $findme);
+  $pos=strpos($mystring, $findme) +1;
 
   if($pos===false)
   {
-    echo "<p>The string " . $findme . " was not found in the string " . $mystring . "</p>";
+    echo "<p>The string " . $findme . " was not found in the string" . $mystring . "</p>";
   } 
   else {
     echo " <p>The string " . $findme . " was found in the string" . $mystring . "</p>";
     echo " and exists at position " . $pos;
   }
 }
+
+//main
+
+findTextinFile("abc","a");
 ?>
