34ff04ef78b4bcf9e0c7a655f5b252c312ef9226
duganlang <113129579+duganlang@users.noreply.github.com>
2022-11-16 12:53:56 -0500

    Update pairC.php

--- pairC.php
+++ pairC.php
@@ -11,4 +11,25 @@ function findTextInFile() {
   }
 }
 
+//function findInText($search)
+{
+  // What to look for
+  $search = 'foo';
+  // Read from file
+  $lines = file('file.txt');
+  // Store true when text is found
+  $pos = strpos($string, $lines);
+
+    // Check if the line contains the string we're looking for, and print if it does
+    if($pos === false)
+    {
+      echo "<p>" ."the string: " . $search . "was not foiund in the string: " . $lines . "</p>";
+    }
+    else 
+    {
+    echo "the string: " . $search . "was found in the string" . $lines;
+    echo " and exists at position $pos";
+    }
+}
+
 ?>
