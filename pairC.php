f7d39ddfaf5e7ea1565e4b9ac4d4d007fe417225
Emmett <113130079+EmmettS03@users.noreply.github.com>
2022-11-16 13:30:37 -0500

    Update pairC.php

--- pairC.php
+++ pairC.php
@@ -2,19 +2,19 @@
 //Pair C - Emmett and Dugan
 
 //function findInText($text) {
-  //What to look for.
+  //What to look for
   $search = '$text';
-  //Read from file.
+  //Read from file
   $lines = file('file.txt');
-  //Store true when text is found.
+  //Store true when text is found
   $pos = strpos($string, $lines);
 
-  //Check if the line contains the string we're looking for, and print if it does.
+  //Check if the line contains the string we're looking for, and print if it does
   if($pos === false) {
     echo "<p>" ."the string: " . $search . "was not foiund in the string: " . $lines . "</p>";
   } else {
     echo "the string: " . $search . "was found in the string" . $lines;
-    echo " and exists at position " . $pos + 1;
+    echo " and exists at position " . $pos;
   }
 }
 ?>
