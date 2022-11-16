abd0895f707d18ffd66c6e48a1d0b4bfb6482ace
Emmett <113130079+EmmettS03@users.noreply.github.com>
2022-11-16 13:26:44 -0500

    Update pairC.php

--- pairC.php
+++ pairC.php
@@ -1,35 +1,21 @@
 <?php
 //Pair C - Emmett and Dugan
 
-//findTextInFile
-function findTextInFile() {
-  $a = "feline cat";
-  if() {
-      echo "<p>String $a not found</p>";
-  } else {
-      echo "<p>$a was found at position 4.</p>";
-  }
-}
-
-//function findInText($text)
-{
-  // What to look for
+//findTextInFile.
+function findTextInFile($text) {
+  //What to look for.
   $search = '$text';
-  // Read from file
+  //Read from file.
   $lines = file('file.txt');
-  // Store true when text is found
+  //Store true when text is found.
   $pos = strpos($string, $lines);
 
-    // Check if the line contains the string we're looking for, and print if it does
-    if($pos === false)
-    {
-      echo "<p>" ."the string: " . $search . "was not foiund in the string: " . $lines . "</p>";
-    }
-    else 
-    {
+  //Check if the line contains the string we're looking for, and print if it does.
+  if($pos === false) {
+    echo "<p>" ."the string: " . $search . "was not foiund in the string: " . $lines . "</p>";
+  } else {
     echo "the string: " . $search . "was found in the string" . $lines;
-    echo " and exists at position $pos";
-    }
+    echo " and exists at position " . $pos + 1;
+  }
 }
-
 ?>
