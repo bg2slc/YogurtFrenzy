ead96ca48f7fd83617e62849a2f167f0e23152ad
Dugan Lang <113129579+duganlang@users.noreply.github.com>
2022-11-23 14:27:58 -0500

    Update pairC.php

--- pairC.php
+++ pairC.php
@@ -5,17 +5,16 @@ require_once("include.php");
 
 //http://localhost/pairC.php
 
+//checks to see if the key f_search(the button) exists so that the code will only run when the button is clicked.
 if(array_key_exists('f_Search', $_POST))
 {
     findTextInFile($_POST['notepad_content'], $_POST['findText']);
 }
-
+//the function to find text in a file or a line of text.
 function findTextInFile($textValue, $findValue)
 {
-
-
+//took awhile because tried to do it without isset and it kept only doing the else. 
     if(isset($_POST['f_checkBox']))
-         
     {
         $pos=strpos($textValue, $findValue);
     }
@@ -29,6 +28,7 @@ function findTextInFile($textValue, $findValue)
     echo "<p>The string " . $findValue . " was not found in the string " . $textValue . "</p>";
   } 
   else {
+  // had to add +1 because the array starts at position 0
     $pos = $pos +1;
     echo " <p>The string " . $findValue . " was found in the string " . $textValue . "</p>";
     echo "<p> and exists at position " . $pos  . "</p>";
