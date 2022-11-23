92f93315e2f2d83180b912a2caffdf07dbd0a479
robin4936 <54117750+robin4936@users.noreply.github.com>
2022-11-23 13:47:25 -0500

    Update pairC.php
    Code after line 46 is my fix for the POS error, put in comments incase it breaks anything

--- pairC.php
+++ pairC.php
@@ -42,3 +42,31 @@ function findTextInFile($textValue, $findValue)
   }
 }
 ?>
+
+////////////
+if(array_key_exists('f_Search', $_POST)) 
+{
+  findTextInFile($_POST['notepad_content'], $_POST['findText']);
+  
+}
+
+function findTextInFile($textValue, $findValue)
+{
+    if ("f_checkBox" === TRUE)
+    {
+        $pos=strpos($textValue, $findValue);
+    }
+    else
+        $pos=stripos($textValue, $findValue);
+
+
+  if($pos===false)
+  {
+    echo "<p>The string " . $findValue . " was not found in the string " . $textValue . "</p>";
+  } 
+  else {
+    $pos = $pos +1;
+    echo " <p>The string " . $findValue . " was found in the string " . $textValue . "</p>";
+    echo "<p> and exists at position " . $pos  . "</p>";
+  }
+}
