71e825d7800211bd91a49ac58a723fb69e900fe5
Emmett <113130079+EmmettS03@users.noreply.github.com>
2022-11-23 14:15:27 -0500

    Update pairC.php

--- pairC.php
+++ pairC.php
@@ -8,7 +8,7 @@ require_once("include.php");
 echo "<form method = POST>";
 
 echo "The text: ";
-displayTextbox("text","test", 30);
+displayTextbox("text", "test", 30);
 echo "<p></p>";
 
 echo "The letter or word you want to search for: ";
@@ -25,15 +25,15 @@ if(array_key_exists('f_Search', $_POST))
 
 function findTextInFile($textValue, $findValue)
 {
-    if ("f_checkBox" == TRUE)
+    if("f_checkBox" == TRUE)
         $pos = strpos($textValue, $findValue);
     else
         $pos = stripos($textValue, $findValue);
 
     if($pos === false) {
-        echo "<p>String " . $findValue . " not found in the string " . $textValue . ".</p>";
+        echo "<p>String " . $findValue . " was not found in " . $textValue . ".</p>";
     } else {
-        echo " <p>String " . $findValue . " was found in the string " . $textValue . "</p>";
+        echo "<p>String " . $findValue . " was found in " . $textValue . "</p>";
         echo "<p> and exists at position " . $pos + 1 . ".</p>";
     }
 }
