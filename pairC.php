fbe1e309d4045b586bc55728befa7f75fdd1a790
Emmett <113130079+EmmettS03@users.noreply.github.com>
2022-11-23 14:08:33 -0500

    Update pairC.php

--- pairC.php
+++ pairC.php
@@ -3,41 +3,39 @@
 require_once("include.php");
 //Pair C - Emmett and Dugan
 
-// http://localhost/pairC.php
+//http://localhost/pairC.php
 
-echo "<form method=POST>";
+echo "<form method = POST>";
 
-echo "The text:";
+echo "The text: ";
 displayTextbox("text","test", 30);
 echo "<p></p>";
 
-echo "The letter or word you want to search for:";
-displayTextbox("text","findText", 10);
+echo "The letter or word you want to search for: ";
+displayTextbox("text", "findText", 10);
 
-displayButton("findButton","Find");
+displayButton("findButton", "Find");
 
 echo "</form>";
 
-if(array_key_exists('f_Search', $_POST)) 
+if(array_key_exists('f_Search', $_POST))
 {
-  findTextInFile($_POST['notepad_content'], $_POST['findText']);
+    findTextInFile($_POST['notepad_content'], $_POST['findText']);
 }
 
 function findTextInFile($textValue, $findValue)
 {
     if ("f_checkBox" == TRUE)
-        $pos=strpos($textValue, $findValue);
+        $pos = strpos($textValue, $findValue);
     else
-        $pos=stripos($textValue, $findValue);
+        $pos = stripos($textValue, $findValue);
 
-  if($pos===false)
-  {
-    echo "<p>The string " . $findValue . " was not found in the string " . $textValue . "</p>";
-  } 
-  else {
-    echo " <p>The string " . $findValue . " was found in the string " . $textValue . "</p>";
-    echo "<p> and exists at position " . $pos +1 . "</p>";
-  }
+    if($pos === false) {
+        echo "<p>String " . $findValue . " not found in the string " . $textValue . ".</p>";
+    } else {
+        echo " <p>String " . $findValue . " was found in the string " . $textValue . "</p>";
+        echo "<p> and exists at position " . $pos + 1 . ".</p>";
+    }
 }
 ?>
 
