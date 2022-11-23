7f6f2e0af33b0f2ca720285afbbfb897d391b9e0
Dugan Lang <113129579+duganlang@users.noreply.github.com>
2022-11-23 10:21:54 -0500

    Update pairC.php

--- pairC.php
+++ pairC.php
@@ -1,6 +1,24 @@
 <?php
 //Pair C - Emmett and Dugan
 
+echo "<form method=POST>";
+
+echo "The text:";
+DisplayTextbox("text","test", 30);
+echo "<p></p>";
+
+echo "The letter or word you want to search for:";
+DisplayTextbox("text","findText", 10);
+
+DisplayButton("findButton","Find");
+
+echo "</form>";
+if(array_key_exists('findButton', $_POST)) 
+{
+  findTextInFile($_POST["test"], $_POST["findText"]);
+  
+}
+
 function findTextInFile($findtext, $text)
 {
   //text area
